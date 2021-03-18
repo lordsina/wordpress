9486d898ca6045a51150e74e37dc4239a3412507
Sina <sinarashidiazar@hotmail.com>
2021-03-18 05:01:54 +0330

    sina

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+ /*
+ * SHOP TEMPLATE AMIN SINA.
+ * 
+ * @pakage ShopSinaAmin
+ * 
+ */
+
+
+ get_header();
+?>
+
+
+    <div class="content">
+        data
+    </div>
+<?php get_footer(); ?>
